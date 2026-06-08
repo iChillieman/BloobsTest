7479e56e9bfea3db7e9423d9a482f1a0b0dcb73f
chillieman <chillieman16@gmail.com>
2026-06-08 18:10:42 -0400

    WIP: Hello World

---
+++ Patches.cs
@@ -0,0 +1,28 @@
+﻿using HarmonyLib;
+
+namespace ChillieFirst
+{
+    [HarmonyPatch]
+    public static class LevelUpPatches
+    {
+        [HarmonyPatch("LevelUpMessage", nameof(LevelUpMessage.ShowMessage))]
+        [HarmonyPrefix]
+        private static bool ShowMessagePrefix(ref string message)
+        {
+            Plugin.Log.LogInfo("ChillieLog - LevelUpMessage.ShowMessage is being called!");
+
+            // Replace "Congratulations!" with "Chillieman!"
+            if (!string.IsNullOrEmpty(message))
+            {
+                if (message.Contains("Congratulations!"))
+                {
+                    message = message.Replace("Congratulations!", "Chillieman!");
+                    Plugin.Log.LogInfo("ChillieLog - Replaced 'Congratulations!' with 'Chillieman!' in level up message");
+                }
+            }
+
+            // Return true = let the original method run with the modified message
+            return true;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace ChillieFirst
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    public class Plugin : BaseUnityPlugin
+    {
+        internal static ManualLogSource Log;
+
+        private void Awake()
+        {
+            Log = Logger;
+            Log.LogInfo("✅ Hello World Mod loaded!");
+
+            var harmony = new Harmony("com.chillieman.helloWorld");
+            harmony.PatchAll();
+        }
+    }
+}
